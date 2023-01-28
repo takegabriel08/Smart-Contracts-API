57a36834fb79622e0719a3b5c50d9cacc8e1ecd1
Gabriel <Gabriel@DESKTOP-7AUAOTD>
2023-01-28 21:28:36 +0200

    final touches

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ const port = process.env.PORT || 3333;
 app.use(express.static('public'))
 app.use(express.json())
 
-app.use('/contracts', contractsRouter)
-
 app.get('/', (req, res) => {
     console.log('Get request from client!')
     res.send('Welcome to the server!')
 })
+
+app.use('/contracts', contractsRouter)
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`)
