856bf53cebf09203c725fd477fc8b9efde37576b
Gabriel <Gabriel@DESKTOP-7AUAOTD>
2023-01-28 20:59:07 +0200

    minor changes

--- routes/contracts.js
+++ routes/contracts.js
@@ -4,15 +4,6 @@ import nedb from "nedb"
 const router = express.Router();
 
 const db = new nedb('database.db')
-// db.insert({
-//     id: "03600f1e-d201-4804-a4a5-0f4269252874",
-//     chainId: 1,
-//     address: "0x23581767a106ae21c074b2276d25e5c3e136a68b",
-//     name: "AvatarsMoonbirds",
-//     symbol: "MOONBIRD",
-//     icon: "MOONBIRD",
-//     createdAtBlock: 14591056
-// })
 db.loadDatabase()
 
 // get all contracts
